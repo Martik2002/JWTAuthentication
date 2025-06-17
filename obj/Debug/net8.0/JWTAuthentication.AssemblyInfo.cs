7b62cd8a6b44ab6dd7651877e7bf929af20a7cb9
Martik Avagyan <avagyanmartik05@gmail.com>
2025-06-17 11:48:24 +0000

    Added gitignor File

--- obj/Debug/net8.0/JWTAuthentication.AssemblyInfo.cs
+++ obj/Debug/net8.0/JWTAuthentication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JWTAuthentication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de48f35f372e15f48e56de8ac4c0a84562acc809")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00f8677a2c6dc9504b41310e245e59bb65c667b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("JWTAuthentication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JWTAuthentication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
